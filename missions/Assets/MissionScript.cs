d68513c6793210417f79ef0ff9ce4041efcd01cc
Kobe Kamin <97008293+BaconThePoro@users.noreply.github.com>
2023-04-02 06:22:58 -0700

    fourth

---
+++ missions/Assets/MissionScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionScript : MonoBehaviour
+{
+    int missionReq;
+    int missionProg;
+    bool missionComplete;
+    Text missionText;
+    public GameObject VictoryScreen = null; 
+
+    void Start()
+    {
+        VictoryScreen.SetActive(false);
+
+        missionReq = Random.Range(4, 10);
+        missionProg = 0;
+        missionComplete = false;
+        missionText = GetComponent<Text>();
+        missionText.text = "Mission: " + missionProg + "/" + missionReq + " Enemies Eliminted";
+    }
+
+    public void UpdateMissionCount()
+    {
+        if (missionComplete || (missionProg + 1) == missionReq)
+        {
+            missionComplete = true;
+            missionText.text = "Mission Completed";
+            VictoryScreen.SetActive(true);
+        }
+        else
+        {
+            missionProg++;
+            missionText.text = "Mission: " + missionProg + "/" + missionReq + " Enemies Eliminted";
+        }
+    }
+}
